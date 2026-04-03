7aee15783dc2dbf61124e11cd76a5b3eb01d6423
inky4832 <inky4832@daum.net>
2026-04-03 11:51:12 +0900

    Add files via upload

---
+++ 01_Java/실습소스/exam10_method2/TestCat.java
@@ -0,0 +1,15 @@
+package exam10_method2;
+
+public class TestCat {
+
+	public static void main(String[] args) {
+
+		// 야옹이 2 암컷
+		Cat c = new Cat("야옹이", 2, "암컷");
+		
+		// c.name="망치";  //접근불가
+		c.setName("망치");
+		String name = c.getName();
+		System.out.println(name);
+	}
+}
