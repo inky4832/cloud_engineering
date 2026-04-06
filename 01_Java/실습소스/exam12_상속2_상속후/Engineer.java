3cdb0e46ba5a3566d62455efe39e27b3264ba1e1
inky4832 <inky4832@daum.net>
2026-04-06 11:48:11 +0900

    Add files via upload

---
+++ 01_Java/실습소스/exam12_상속2_상속후/Engineer.java
@@ -0,0 +1,24 @@
+package exam12_상속2_상속후;
+
+//엔지니어
+// Engineer is a Employee (상속관계)
+public class Engineer extends Employee{
+
+	String skill; // 엔지니어의 기술
+	
+	public Engineer() {}
+	public Engineer(String name, int salary, String skill) {
+		System.out.println("Enginner 생성자");
+		this.name = name;
+		this.salary = salary;
+		this.skill = skill;
+	}
+	
+	//getter
+	//setter
+	
+	public String getEngineer() {
+		return name+"\t"+salary+"\t"+skill;
+	}
+
+}
---
+++ 01_Java/실습소스/exam12_상속2_상속후/Manager.java
@@ -0,0 +1,23 @@
+package exam12_상속2_상속후;
+
+// 관리자
+// Manager is a Employee (상속관계)
+public class Manager extends Employee{
+
+	String depart; // 관리부서
+	
+	public Manager() {}
+
+	public Manager(String name, int salary, String depart) {
+		System.out.println("Manager 생성자");
+		this.name = name;
+		this.salary = salary;
+		this.depart = depart;
+	}
+	
+	//getter
+	//setter
+	public String getManager() {
+		return name+"\t"+salary+"\t"+depart;
+	}
+}
