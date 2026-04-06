cb10222f88fdd1d4c060d1c6d88d1e8c0cfd3fdb
inky4832 <inky4832@daum.net>
2026-04-06 13:51:11 +0900

    Add files via upload

---
+++ 01_Java/실습소스/exam12_상속3_super/Manager.java
@@ -0,0 +1,32 @@
+package exam12_상속3_super;
+
+// 관리자
+// Manager is a Employee (상속관계)
+public class Manager extends Employee{
+
+	
+
+	String depart; // 관리부서
+	
+	//생성자 작성을 generate으로 ........
+	public Manager() {
+		super();
+	}
+
+	public Manager(String name, int salary) {
+		super(name, salary);
+	}
+
+	public Manager(String name, int salary, String depart) {
+		super(name, salary);
+		this.depart = depart;
+	}
+
+	//getter
+	//setter
+	public String getManager() {
+		return name+"\t"+salary+"\t"+depart;
+	}
+
+	
+}
