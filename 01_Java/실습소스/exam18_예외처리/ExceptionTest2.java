855330e50f36de048d46fcffdab4dbb5861f1fdd
inky4832 <inky4832@daum.net>
2026-04-09 13:54:35 +0900

    Add files via upload

---
+++ 01_Java/실습소스/exam18_예외처리/ExceptionTest2.java
@@ -0,0 +1,19 @@
+package exam18_예외처리;
+
+public class ExceptionTest2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("프로그램 시작");	
+	    try {
+		 int n=0;
+		 int result = 10/n;  
+		 System.out.println("결과:"+result);
+	    }catch(ArithmeticException e) {
+	    	//예외처리 코드
+	    	System.out.println("0으로 나누어서 예외발생");
+	    }
+	    
+		System.out.println("프로그램 정상종료");	
+	}//end main
+}
