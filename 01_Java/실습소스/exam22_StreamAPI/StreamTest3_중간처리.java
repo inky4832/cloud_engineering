812c41f40d6adac0e309ac3af7f4b1daf40a9a20
inky4832 <inky4832@daum.net>
2026-04-13 13:41:37 +0900

    Add files via upload

--- 01_Java/실습소스/exam22_StreamAPI/StreamTest3_중간처리.java
+++ 01_Java/실습소스/exam22_StreamAPI/StreamTest3_중간처리.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class StreamTest3_중간처리 {
@@ -29,7 +30,7 @@ public class StreamTest3_중간처리 {
 		stream.forEach(System.out::println);
 		System.out.println("============================");
 		
-		//중간처리 - 중복제거 distinct()
+		//중간처리1 - 중복제거 distinct()
 //		stream = list.stream();
 //		Stream<String> stream2 = stream.distinct();
 //		stream2.forEach(System.out::println);
@@ -37,6 +38,34 @@ public class StreamTest3_중간처리 {
 		list.stream()
 		    .distinct()
 		    .forEach(System.out::println);
+		System.out.println("============================");
 		
+		//중간처리2 - 필터링 - filter(Predicate)
+		stream = list.stream();
+		
+		//익명클래스
+		Predicate<String> p = new Predicate<String>() {
+			@Override
+			public boolean test(String s) {
+				return s.length() == 3;
+			}
+		};
+		//람다표현식
+		Predicate<String> p2 = s -> s.length() == 3;
+		Stream<String> stream2 = stream.filter(p2);
+		stream2.forEach(System.out::println);
+		
+		//최종 코드
+		list.stream()
+		    .filter(s -> s.length() == 3)
+		    .forEach(System.out::println);
+		
+		System.out.println("============================");
+		// distinct() + filter(()
+		//최종 코드
+		list.stream()
+		    .distinct()
+		    .filter(s -> s.length() == 3)
+		    .forEach(System.out::println);
 	}
 }
