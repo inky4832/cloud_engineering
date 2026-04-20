550c23899514f486f7845fcd42b248fc9f2f3d0d
inky4832 <inky4832@daum.net>
2026-04-20 13:22:41 +0900

    Add files via upload

---
+++ 04_JDBC/실습소스/exam1/SelectTest2.java
@@ -0,0 +1,61 @@
+package exam1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class SelectTest2 {
+
+	public static void main(String[] args) {
+
+		// Dept 테이블 조회
+		//1. 4가지 정보 
+	     String driver = "com.mysql.cj.jdbc.Driver"; // 핵심이 되는 클래스명
+	     String url = "jdbc:mysql://localhost:3306/testdb";
+	     String userid ="root";
+	     String passwd = "1234";
+		
+		//2. 드라이버 로딩
+	     
+	     try {
+			Class.forName(driver);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	     
+	    //3. Connection 맺기
+	     Connection con = null;
+	     PreparedStatement pstmt = null;
+	     ResultSet rs = null;
+	     try {
+			 con = DriverManager.getConnection(url, userid, passwd);
+			 String sql = "select deptno, dname, loc from dept";
+			 pstmt = con.prepareStatement(sql);
+			 rs = pstmt.executeQuery();
+				while(rs.next()) { // 행선택
+					//열선택
+					int deptno = rs.getInt("deptno");
+					String dname = rs.getString("dname");
+					String loc = rs.getString("loc");
+					System.out.printf("%d, %s , %s \n", deptno, dname,loc);
+				}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				  rs.close();
+				  pstmt.close();
+			      con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}// try~catch~finally
+	}//end main
+}//end class
+
+
+
+
